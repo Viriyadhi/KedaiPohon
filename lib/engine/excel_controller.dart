f0aa2f10a6f466c297711ba39abc749dda8c3a06
∀rkadia Eden <bimo.17168@gmail.com>
2024-08-10 19:12:50 +0700

    feat[RR]:Add Read Function

--- lib/engine/excel_controller.dart
+++ lib/engine/excel_controller.dart
@@ -82,5 +82,68 @@ class ExcelController{
       );
     }
   }
+
+  Future<void> readDataExcel(String koordinasi, BuildContext context) async{
+    try {
+      var directory = await getDownloadsDirectory();
+      String dirPath = directory!.path;
+      var file = '$dirPath/ga_logis.xlsx';
+      var bytes = File(file).readAsBytesSync();
+      var excel = Excel.decodeBytes(bytes);
+
+      for (var table in excel.tables.keys){
+        print(table);
+        print(excel.tables[table]!.maxColumns);
+        print(excel.tables[table]!.maxRows);
+        for (var row in excel.tables[table]!.rows){
+          for (var cell in row){
+            final value = cell?.value;
+            final numFormat = cell?.cellStyle!.numberFormat ?? NumFormat.standard_0;
+            switch(value){
+              case null:
+                print('  empty cell');
+                print('  format: $numFormat');
+              case TextCellValue():
+                print('  text: ${value.value}');
+              case FormulaCellValue():
+                print('  formula: ${value.formula}');
+                print('  format: $numFormat');
+              case IntCellValue():
+                print('  int: ${value.value}');
+                print('  format: $numFormat');
+              case BoolCellValue():
+                print('  bool: ${value.value ? 'YES!!' : 'NO..' }');
+                print('  format: $numFormat');
+              case DoubleCellValue():
+                print('  double: ${value.value}');
+                print('  format: $numFormat');
+              case DateCellValue():
+                print('  date: ${value.year} ${value.month} ${value.day} (${value.asDateTimeLocal()})');
+              case TimeCellValue():
+                print('  time: ${value.hour} ${value.minute} ... (${value.asDuration()})');
+              case DateTimeCellValue():
+                print('  date with time: ${value.year} ${value.month} ${value.day} ${value.hour} ... (${value.asDateTimeLocal()})');
+            }
+            print('$row');
+          }
+        }
+      }
+    } catch (e) {
+      AlertDialog(
+        title: const Text('Error'),
+        content: const Text('Data tidak dapat di ditampilkan'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: const Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'),
+          ),
+        ],
+      );
+    }
+  }
 }
 
