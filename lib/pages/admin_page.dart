09d86a5657f535023c6bd344fe86ac1d5bd1b3e7
Viriyadhi <viriyadhitjan@gmail.com>
2024-09-26 11:54:46 +0800

    WIP[VS]: admin page layouting

--- lib/pages/admin_page.dart
+++ lib/pages/admin_page.dart
@@ -10,6 +10,93 @@ class AdminPage extends StatefulWidget {
 class _AdminPageState extends State<AdminPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Row(
+        children: [
+          SizedBox(
+            width: screenWidth * 0.25,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 140,
+                    height: 180,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                    ),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/images/kedai_pohon.png',
+                      ),
+                    ),
+                  ),
+                  const Text('ASEP'),
+                  const Text("Admin"),
+                  Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(145, 180, 0, 1),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        shadowColor: Colors.transparent,
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'History Transaksi',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(145, 180, 0, 1),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        shadowColor: Colors.transparent,
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Stock Barang',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // Right side layout, which will take the remaining 80%
+          Expanded(
+            child: Text("Hi")
+          ),
+        ],
+      ),
+    );
   }
 }
+
